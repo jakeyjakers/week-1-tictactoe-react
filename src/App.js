dee18c6a357f20758ae772010bdcb65d955205a4
Jacob Eichler <jeichler7@gmail.com>
2022-09-22 13:10:42 -0600

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import logo from './logo.svg';
 import { useState } from 'react';
-import './App.css';
+import './TicTacToe.css'
 import Square from './Square';
 
 function App() {
 const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', ''])
 const [player, setPlayer] = useState(true)
-  const string = 'Hello'
+  
+  const handleClick = () => {
+    setSquares(['', '', '', '', '', '', '', '', ''])
+    setPlayer(true)
+  }
+
+  const calculateWinner = (arr) => {
+    let lines = [ [0, 1, 2],[3, 4, 5], [6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6],]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      //arr[a] === arr[b] && arr[a] === arr[c]
+      if ( arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} won!`
+      }
+  
+    }
+      return `Who will win?`
+    
+  }
+// need to import css
   return (
     <div className="App">
-      <Square
-        squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer}
-      />
+      <span>{calculateWinner(squares)}</span>
+      
+      <div className='container'>
+          {squares.map((value, index) => {
+            return (
+            <Square
+            squares={squares}
+            setSquares={setSquares}
+            player={player}
+            setPlayer={setPlayer}
+            squareValue={value}
+            index={index}
+          />
+            )
+          })}
+    
+      </div>
+      <button onClick={handleClick}>
+        Reset
+      </button>
     </div>
   );
 }
